c6da217baa11a236cae9b3a4eda473a7b2514dda
Rafael <rafaelbruno210@gmail.com>
2024-02-21 20:57:51 -0300

    Alterações após entrega do desafio para deixar o código mais legível.

--- DesafioMjolnir/DesafioMjolnir/src/Mjolnir.java
+++ DesafioMjolnir/DesafioMjolnir/src/Mjolnir.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Mjolnir {
     public static void main(String[] args){
 		Scanner meuScanner = new Scanner(System.in);
+		System.out.println("Quantas vezes deseja testar? ");
 		int C = meuScanner.nextInt();
 		String nome;
 		int N;
 		for (int i = 0; i < C ; i++) {
+			System.out.println("Quem irá tentar levantar? ");
 			nome = meuScanner.next();
+			System.out.println("Qual força em Newtons? ");
 			N = meuScanner.nextInt();
+			System.out.println("Conseguiu levantar? Y == Sim N == Não");
 			if (nome.equals("Thor")) System.out.println("Y");
 			else System.out.println("N");
 		}
